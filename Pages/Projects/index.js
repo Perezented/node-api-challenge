d5f1d0a6ed28e2a0de7114293e41121bac79a1a9
Michael Perez <mperez6473@gmail.com>
2020-06-05 11:29:59 -0500

    Able to get the actions of a specific project. Sent a test project as well.

--- Pages/Projects/index.js
+++ Pages/Projects/index.js
@@ -4,10 +4,26 @@ const router = express.Router();
 
 const Projects = require("../../data/helpers/projectModel");
 
+function validateProject(req, res, next) {
+    if (req.body.name || req.body.description) {
+        next();
+    } else res.status(400).json({ error: "Missing name or description" });
+}
+
 router.get("/", (req, res) => {
     Projects.get().then((projects) => {
         res.status(200).json(projects);
     });
+});
+
+router.get("/:id/actions", (req, res) => {
+    Projects.getProjectActions(req.params.id).then((project) => {
+        res.status(200).json(project);
+    });
+});
+
+router.post("/", validateProject, (req, res) => {
+    Projects.insert(req.body).then(res.status(203).json(req.body));
 });
 
 module.exports = router;
