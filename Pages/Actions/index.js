0afaa21d19c4456c8bb07e7ba8493606e1ba3b4a
Michael Perez <mperez6473@gmail.com>
2020-06-05 11:42:33 -0500

    Sent an aciton

--- Pages/Actions/index.js
+++ Pages/Actions/index.js
@@ -10,4 +10,18 @@ router.get("/", (req, res) => {
     });
 });
 
+function validateAction(req, res, next) {
+    if (!req.body.description || !req.body.notes || !req.body.project_id) {
+        res.status(400).json({
+            error: "Description, Notes, and Project Id is required",
+        });
+    } else if (req.body.description.length > "128") {
+        res.status(400).json({ error: "Description max length of 128" });
+    } else next();
+}
+
+router.post("/", validateAction, (req, res) => {
+    Actions.insert(req.body).then(res.status(203).json(req.body));
+});
+
 module.exports = router;
