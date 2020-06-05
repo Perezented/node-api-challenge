28198342ea790cd360017f21af01625efcf59a84
Michael Perez <mperez6473@gmail.com>
2020-06-05 12:03:15 -0500

    Have the Project updating now as well.

--- Pages/Projects/index.js
+++ Pages/Projects/index.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const Projects = require("../../data/helpers/projectModel");
 
 function validateProject(req, res, next) {
-    if (req.body.name || req.body.description) {
-        next();
-    } else res.status(400).json({ error: "Missing name or description" });
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({ error: "Missing name or description" });
+    } else next();
 }
 
 router.get("/", (req, res) => {
@@ -30,6 +30,13 @@ router.delete("/:id", (req, res) => {
     Projects.remove(req.params.id).then((number) => {
         res.status(204).json(number);
     });
+});
+
+router.put("/:id", validateProject, (req, res) => {
+    req.body.id = req.params.id;
+    Projects.update(req.params.id, req.body).then(
+        res.status(203).json(req.body)
+    );
 });
 
 module.exports = router;
